a6a65edbdce4ad84f886158c74acdcb08f6e3a4f
isurusiri <isuruanatomy@gmail.com>
2019-01-17 13:54:23 +0530

    initial source code

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>
+        This is React Starter{" "}
+        <span aria-label="ReactJS" role="img">
+          ⚛️
+        </span>
+      </h1>
+      <p>
+        Change &ldquo;src/App.js&rdquo; and save to make your cool React app.
+      </p>
+      <a
+        className="App-link"
+        href="https://github.com/isurusiri/react-starter"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more about React Starter
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
